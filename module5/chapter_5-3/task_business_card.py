f13334d30c5a5d135a40d92a3d5e60efdc368056
nataliagwardjan <natalia.gwardjan@gmail.com>
2025-08-29 07:17:17 +0200

    correct task business_card

--- module5/chapter_5-3/task_business_card.py
+++ module5/chapter_5-3/task_business_card.py
@@ -1,27 +1,18 @@
 from faker import Faker
 import datetime
-import random
 
-fake = Faker()
+fake = Faker("pl_PL")
 
-e_mail_ending = ["gmail.com", "wp.pl", "op.pl", "edu.pl", "mail.com"]
-company_list = ["ABC", "Samsung", "Allegro", "Simens", "Bosch", "Empik", "Ikea"]
-position_list = ["Sales Engineer", "Software Developer", "General Director", "Accountant", "IT Consultant",
-                 "HR Specialist", "Technical Support"]
-
-
-class BaseContact():
+class BaseContact:
     def __init__(self, name, surname, e_mail, phone):
         self.name = name
         self.surname = surname
         self.e_mail = e_mail
         self.phone = phone
 
-        self._label_length = len(self.name) + len(self.surname) + 1
-
     @property
     def label_length(self):
-        return self._label_length
+        return len(self.name) + len(self.surname) + 1
 
     def contact(self):
         return f"I dial the number {self.phone} and call to {self.name} {self.surname}"
@@ -45,20 +36,28 @@ class BusinessContact(BaseContact):
 
 
 def create_contact(contact_type):
-    person = fake.name().split(" ")
-    phone = "+48" + str(random.randrange(100000000, 1000000000))
-    business_phone = "+48" + str(random.randrange(100000000, 1000000000))
-    company = random.choice(company_list)
-    if (issubclass(contact_type, BusinessContact)):
-        return BusinessContact(name=person[0], surname=person[1], company=company,
-                               position=random.choice(position_list),
-                               e_mail=f"{person[0].lower()}.{person[1].lower()}@{company.lower()}.com", phone=phone,
-                               business_phone=business_phone)
-    else:
-        return BaseContact(name=person[0], surname=person[1],
-                           e_mail=f"{person[0].lower()}.{person[1].lower()}@{random.choice(e_mail_ending)}",
-                           phone=phone)
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    phone = fake.phone_number()
+    company = fake.company()
 
+    if issubclass(contact_type, BusinessContact):
+        return BusinessContact(
+            name=first_name,
+            surname=last_name,
+            company=company,
+            position=fake.job(),
+            e_mail=fake.company_email(),
+            phone=phone,
+            business_phone=fake.phone_number()
+        )
+    else:
+        return BaseContact(
+            name=first_name,
+            surname=last_name,
+            e_mail=fake.email(),
+            phone=phone
+        )
 
 def creation_time(function):
     def inter_function(*args):
