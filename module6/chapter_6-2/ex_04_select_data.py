ecb4902dc238243d4b47bf6bc6615936f4fa3253
nataliagwardjan <natalia.gwardjan@gmail.com>
2025-10-06 10:24:51 +0200

    sglite3 exercises

---
+++ module6/chapter_6-2/ex_04_select_data.py
@@ -0,0 +1,105 @@
+import sqlite3
+
+def create_connection(db_file):
+   """ create a database connection to the SQLite database
+       specified by db_file
+   :param db_file: database file
+   :return: Connection object or None
+   """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       return conn
+   except sqlite3.Error as e:
+       print(e)
+   return conn
+
+def add_project(conn, project):
+   """
+   Create a new project into the projects table
+   :param conn:
+   :param project:
+   :return: project id
+   """
+   sql = '''INSERT INTO projects(nazwa, start_date, end_date)
+             VALUES(?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, project)
+   conn.commit()
+   return cur.lastrowid
+
+def add_task(conn, task):
+   """
+   Create a new task into the tasks table
+   :param conn:
+   :param task:
+   :return: task id
+   """
+   sql = '''INSERT INTO tasks(project_id, nazwa, opis, status, start_date, end_date)
+             VALUES(?,?,?,?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, task)
+   conn.commit()
+   return cur.lastrowid
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+
+if __name__ == "__main__":
+   project = ("Powtórka z angielskiego", "2020-05-11 00:00:00", "2020-05-13 00:00:00")
+
+   conn = create_connection("database.db")
+   pr_id = add_project(conn, project)
+
+   task = (
+       pr_id,
+       "Czasowniki regularne",
+       "Zapamiętaj czasowniki ze strony 30",
+       "started",
+       "2020-05-11 12:00:00",
+       "2020-05-11 15:00:00"
+   )
+
+   task_id = add_task(conn, task)
+
+   select_all_projects = select_all(conn, "projects")
+   select_all_tasks = select_all(conn, "tasks")
+
+   print(select_all_projects)
+   print(select_all_tasks)
+
+   select_task_id_1 = select_where(conn, "tasks", project_id=1)
+
+   print(select_task_id_1)
+
+   print(pr_id, task_id)
+   conn.commit()
---
+++ module6/chapter_6-2/ex_05_update.py
@@ -0,0 +1,105 @@
+import sqlite3
+
+def create_connection(db_file):
+   """ create a database connection to the SQLite database
+       specified by db_file
+   :param db_file: database file
+   :return: Connection object or None
+   """
+   conn = None
+   try:
+       conn = sqlite3.connect(db_file)
+       return conn
+   except sqlite3.Error as e:
+       print(e)
+   return conn
+
+def add_project(conn, project):
+   """
+   Create a new project into the projects table
+   :param conn:
+   :param project:
+   :return: project id
+   """
+   sql = '''INSERT INTO projects(nazwa, start_date, end_date)
+             VALUES(?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, project)
+   conn.commit()
+   return cur.lastrowid
+
+def add_task(conn, task):
+   """
+   Create a new task into the tasks table
+   :param conn:
+   :param task:
+   :return: task id
+   """
+   sql = '''INSERT INTO tasks(project_id, nazwa, opis, status, start_date, end_date)
+             VALUES(?,?,?,?,?,?)'''
+   cur = conn.cursor()
+   cur.execute(sql, task)
+   conn.commit()
+   return cur.lastrowid
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query tasks from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
+if __name__ == "__main__":
+   conn = create_connection("database.db")
+   update(conn, "tasks", 2, status="started")
+   update(conn, "tasks", 2, status="started")
+   conn.close()
